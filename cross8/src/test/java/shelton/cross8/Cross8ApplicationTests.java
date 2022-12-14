82034b0cc72a7446ae3ab131b62c966f52de11a7
Ashley Shelton <63636009+jjtlemall@users.noreply.github.com>
2022-12-14 10:53:50 -0600

    Add files via upload
    cross8

---
+++ cross8/src/test/java/shelton/cross8/Cross8ApplicationTests.java
@@ -0,0 +1,13 @@
+package shelton.cross8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cross8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
